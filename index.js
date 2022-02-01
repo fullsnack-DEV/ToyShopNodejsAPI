8b76c485deda2f5c2277b4c78dda0b79ce8ee3e2
pratik <pratikpatre2019@gmail.com>
2022-02-02 01:31:17 +0530

    Added the Databse Connections

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello")
+const express = require("express");
+
+const app = express();
+
+const dotenv = require("dotenv");
+
+//env config
+dotenv.config();
+
+//gettig momgoose
+const mongoose = require("mongoose");
+
+//Playing with the Router
+
+//Connecting application to the Mongo db Atlas Server
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Success db Connection"))
+  .catch((err) => console.log(err));
+
+//listining
+app.listen(process.env.PORT_NO || 5000, () => {
+  console.log("Listning from Backend . . .");
+});
