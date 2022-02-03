87ba42f83eac899811323ac0f8f0d8e5aac308b3
pratik <pratikpatre2019@gmail.com>
2022-02-03 15:05:17 +0530

    Creating the User Roles

---
+++ Middleware/VerifyToken.js
@@ -0,0 +1,34 @@
+//Verification of jwt Middleware
+const jwt = require("jsonwebtoken");
+
+const VerifyToken = (req, res, next) => {
+  const authheader = req.headers.token;
+  if (authheader) {
+    const token = authheader.split(" ")[1];
+
+    //if token preset then we will Match it to our user token
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("Token is not Valid! ");
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(400).json({
+      message: "You are not Authenticated ",
+    });
+  }
+};
+
+const VerifyTokenandAuthrozation = (req, res, next) => {
+  VerifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isadmin) {
+      next();
+    } else {
+      res.status(403).json({
+        message: "you are not allowed",
+      });
+    }
+  });
+};
+
+module.exports = { VerifyToken, VerifyTokenandAuthrozation };
--- Router/UserRoute.js
+++ Router/UserRoute.js
@@ -1,9 +1,46 @@
+const Cryptojs = require("crypto-js");
+const User = require("../Models/UserModel");
+
+const { VerifyTokenandAuthrozation } = require("../Middleware/VerifyToken");
+
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-  res.send({
-    message: "User test us successful",
-  });
-});
+router.put("/:id", VerifyTokenandAuthrozation, async (req, res) => {
+  console.log(req.params.id);
 
+  if (req.body.password) {
+    req.body.password = Cryptojs.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_MESSAGE
+    ).toString();
+  }
+
+  try {
+    //update the usetr
+    const updateduser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      {
+        new: true,
+      }
+    );
+
+    res.status(200).json({
+      message: "Suucess",
+      data: {
+        updateduser,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: Error,
+      error,
+    });
+  }
+});
 module.exports = router;
+
+//Acces token
+//"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZmFjMzhmZGQ1NDBiMzYxMzM4MjRhZSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NDM4Nzk0MDUsImV4cCI6MTY0NDEzODYwNX0.4RH26thUpA_i6lNASLnVm1OIz2lY1M_eVCWOfhc4jto"
