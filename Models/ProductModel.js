7ded72840293efa9a16ac0545018b46f73b86d1a
pratik <pratikpatre2019@gmail.com>
2022-02-15 23:30:37 +0530

    Changing the Product Schema

--- Models/ProductModel.js
+++ Models/ProductModel.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const ProductScehma = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
+    label: { type: String, unique: true },
     desc: { type: String, required: true },
     size: { type: String },
     cateogries: [
@@ -13,6 +14,7 @@ const ProductScehma = new mongoose.Schema(
       },
     ],
     color: { type: String },
+    size: { type: String },
     price: { type: Number, required: true },
     img: {
       type: Object,
@@ -33,3 +35,20 @@ const ProductScehma = new mongoose.Schema(
 //exporting the Model
 //mongoose.model will create a model on the basis of the UserSchema
 module.exports = mongoose.model("Product", ProductScehma);
+
+//product Schema
+
+// Toys Data
+//Name ==> label ==> optional
+//Price
+//Size in cm
+//MAde of Plastic
+
+//Image
+//Desc
+//cateogries
+
+//Another Route for Most polpular as well
+//One route to Show the offers ==> Work on it a bit
+//one route for the onboarding screen
+//
